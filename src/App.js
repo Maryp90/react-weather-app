dc2f00d63e486c40223b84d9db2fd9b14bb93578
Mariana Pereira <mariana.p.azevedopereira@gmail.com>
2023-09-17 15:53:56 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Mariana Pereira and is {""}
-        <a href="https://github.com/Maryp90/react-weather-app" target="_blank" rel="noreferrer">open-sourced</a> on Github
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Mariana Pereira and is {""}
+          <a href="https://github.com/Maryp90/react-weather-app" target="_blank" rel="noreferrer">open-sourced</a> on Github
+        </footer>
+      </div>
     </div>
   );
 }
