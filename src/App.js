e9a7f77aff9e4f7e12563c005dfcdc1223cd2461
Mariana Pereira <mariana.p.azevedopereira@gmail.com>
2023-09-17 15:44:20 +0100

    Added the Github repo url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Mariana Pereira and is {""}
+        <a href="https://github.com/Maryp90/react-weather-app" target="_blank" rel="noreferrer">open-sourced</a> on Github
+      </footer>
     </div>
   );
 }
