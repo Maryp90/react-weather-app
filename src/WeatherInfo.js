c061299cca1648cf33dd44c8efdd4e6c57078ab5
Mariana Pereira <mariana.p.azevedopereira@gmail.com>
2023-09-24 13:05:51 +0100

    made some CSS changes

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -17,8 +17,8 @@ export default function WeatherInfo(props) {
                     </div>
                     <div className="weather-conditions">
                         <ul>
-                            <li>Humidity: {props.data.humidity}%</li>
-                            <li>Wind: {props.data.wind} km/h</li>
+                            <li>Humidity: <strong>{props.data.humidity}%</strong></li>
+                            <li>Wind: <strong>{props.data.wind} km/h</strong></li>
                         </ul>
                     </div>
 
